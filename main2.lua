bd62c35139a81a5cc06919ab99830250d056dea4
Ocawesome101 <ocawesome101@gmail.com>
2024-01-02 14:37:28 -0500

    some more gui shenanigans

--- main2.lua
+++ main2.lua
@@ -6,6 +6,8 @@ local fl = require("moonfltk")
 local linda1 = lanes.linda()
 local device = (...)
 
+local KEY = "SynthesisPlayground"
+
 local synth_thread = lanes.gen("*", function() 
   local util = require("synth.util")
   local midi = require("synth.midi")
@@ -22,7 +24,7 @@ local synth_thread = lanes.gen("*", function()
     local pedal, pressed = e[8][5], e[8][6]
     if pedal == PEDAL_LOOP then
       inLoop = pressed > 63
-      linda1:send("set", "inLoop", inLoop)
+      linda1:send(KEY, "inLoop", inLoop)
     end
   end)
 
@@ -38,7 +40,39 @@ if synth.status == "error" then
   return
 end
 
+local MARGIN = 10
+
 local win = fl.double_window(640, 480, "Synthesis Playground")
+fl.font(fl.COURIER)
+
+local winw, winh = win:w(), win:h()
+
+local menubar = fl.menu_bar(0, 0, winw, fl.height(), "A")
+
+
+local _x, _y, _w, _h = fl.text_extents("Loop")
+local flasher = fl.box('down box', winw-_w-MARGIN, fl.height(), _w+MARGIN, fl.height(), "Loop")
+flasher:color(0x44000000)
+flasher:labelcolor(0xCC000000)
+
+menubar:add("File")
+
+flasher:show()
+
 win:done()
 win:show()
-fl.run()
+
+while fl.wait() do
+  repeat
+    local k, v, v2 = linda1:receive(0, KEY)
+    if k and v == "inLoop" then
+      if v2 then
+        flasher:color(fl.rgb_color(0xCC, 0, 0))
+        flasher:labelcolor(fl.rgb_color(0x44, 0, 0))
+      else
+        flasher:color(fl.rgb_color(0x44, 0, 0))
+        flasher:labelcolor(fl.rgb_color(0xCC, 0, 0))
+      end
+    end
+  until not k
+end
