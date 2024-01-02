2a6b69cce3f4db1cca18b1f47bca3bb0c08bd841
Ocawesome101 <ocawesome101@gmail.com>
2024-01-02 13:54:48 -0500

    little bit of test midi code

--- main2.lua
+++ main2.lua
@@ -10,9 +10,21 @@ local synth_thread = lanes.gen("*", function()
   local util = require("synth.util")
   local midi = require("synth.midi")
 
+  local PEDAL_SUSTAIN, PEDAL_LOOP = 64, 67
+
   if device then
     midi.init(device)
   end
+
+  local inLoop = false
+
+  midi.handle("SND_SEQ_EVENT_CONTROLLER", function(e)
+    local pedal, pressed = e[8][5], e[8][6]
+    if pedal == PEDAL_LOOP then
+      inLoop = pressed > 63
+      linda1:send("set", "inLoop", inLoop)
+    end
+  end)
 
   while true do
     midi.tick()
